091a9cbf07d01a20ba31a289f97025b20097909f
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2021-02-07 16:52:16 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FL(a,b) for(int i=a;i<b;i++)
+#define ITRL(x) for(auto itr = x.begin();itr!=x.end();itr++)
+#define ll long long int
+#define PB push_back
+#define EB emplace_back
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
