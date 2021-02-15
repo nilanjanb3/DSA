87ff6a59ff63b54b3631c5e8d1946c696cbd1a23
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2021-02-15 12:48:53 +0530

    Update and rename main.cpp to boilerPlate.cpp

--- boilerPlate.cpp
+++ boilerPlate.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-#define FL(a,b) for(int i=a;i<b;i++)
+#define FLI(a,b) for(int i=a;i<b;i++)
+#define FLJ(a,b) for(int j=a;j<b;j++)
 #define ITRL(x) for(auto itr = x.begin();itr!=x.end();itr++)
 #define ll long long int
 #define PB push_back
@@ -9,6 +10,6 @@ using namespace std;
 
 int main()
 {
-    
+
     return 0;
 }
